2ae92f4216992030c880bbf92740420594728bf6
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 <fisher.ziv@gmail.com>
2023-01-15 10:03:17 +0200

    base1

--- client/src/components/SideBar.jsx
+++ client/src/components/SideBar.jsx
@@ -30,7 +30,7 @@ const SideBar = () => {
           <path d="M15 4V3H9v1H4v2h1v13c0 1.1.9 2 2 2h10c1.1 0 2-.9 2-2V6h1V4h-5zm2 15H7V6h10v13z"></path>
           <path d="M9 8h2v9H9zm4 0h2v9h-2z"></path>
         </svg>
-        <h6>אשפה</h6>
+        <h6>אשdפה</h6>
       </div>
     </div>
   );
