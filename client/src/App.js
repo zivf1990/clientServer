7c6d8f69ea8afb215407b3c42b6be3a708de4d57
Michael <Michaelsalem720@gmail.com>
2023-01-15 15:03:43 +0200

    v2

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,6 @@
 import "./App.css";
 import "./style/main.css";
 import SideBar from "./components/SideBar";
-import Main from "./components/Main";
-import { useEffect } from "react";
 import UserFiles from "./components/UserFiles";
 
 function App() {
@@ -10,7 +8,6 @@ function App() {
     <div className="App">
       <UserFiles />
       <SideBar />
-      <Main />
     </div>
   );
 }
